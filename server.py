69f5fd1feba3b000e5a179f722d9068658d3031a
ruslan-panda <rusruss1003@mail.ru>
2024-03-20 16:08:58 +0300

    123

--- server.py
+++ server.py
@@ -1,16 +1,23 @@
 from flask import Flask, request, jsonify
 import logging
+import random
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
+
+cities = {
+    'РјРѕСЃРєРІР°': ['1540737/daa6e420d33102bf6947', '213044/7df73ae4cc715175059e'],
+    'РЅСЊСЋ-Р№РѕСЂРє': ['1652229/728d5c86707054d4745f', '1030494/aca7ed7acefde2606bdc'],
+    'РїР°СЂРёР¶': ["1652229/f77136c2364eb90a3ea8", '123494/aca7ed7acefd12e606bdc']
+}
 
 sessionStorage = {}
 
 
 @app.route('/post', methods=['POST'])
 def main():
-    logging.info(f'Request: {request.json!r}')
+    logging.info('Request: %r', request.json)
     response = {
         'session': request.json['session'],
         'version': request.json['version'],
@@ -18,95 +25,113 @@ def main():
             'end_session': False
         }
     }
-    handle_dialog(request.json, response)
-    logging.info(f'Response:  {response!r}')
+    handle_dialog(response, request.json)
+    logging.info('Response: %r', response)
     return jsonify(response)
 
-def handle_dialog(req, res):
+
+def handle_dialog(res, req):
     user_id = req['session']['user_id']
-
     if req['session']['new']:
+        res['response']['text'] = 'РџСЂРёРІРµС‚! РќР°Р·РѕРІРё СЃРІРѕС‘ РёРјСЏ!'
         sessionStorage[user_id] = {
-            'suggests': [
-                "Не хочу.",
-                "Не буду.",
-                "Отстань!",
+            'first_name': None,
+            'game_started': False
+        }
+        return
+
+    if sessionStorage[user_id]['first_name'] is None:
+        first_name = get_first_name(req)
+        if first_name is None:
+            res['response']['text'] = 'РќРµ СЂР°СЃСЃР»С‹С€Р°Р»Р° РёРјСЏ. РџРѕРІС‚РѕСЂРё, РїРѕР¶Р°Р»СѓР№СЃС‚Р°!'
+        else:
+            sessionStorage[user_id]['first_name'] = first_name
+            sessionStorage[user_id]['guessed_cities'] = []
+            res['response']['text'] = f'РџСЂРёСЏС‚РЅРѕ РїРѕР·РЅР°РєРѕРјРёС‚СЊСЃСЏ, {first_name.title()}. РЇ РђР»РёСЃР°. РћС‚РіР°РґР°РµС€СЊ РіРѕСЂРѕРґ РїРѕ С„РѕС‚Рѕ?'
+            res['response']['buttons'] = [
+                {
+                    'title': 'Р”Р°',
+                    'hide': True
+                },
+                {
+                    'title': 'РќРµС‚',
+                    'hide': True
+                }
             ]
-        }
-        res['response']['text'] = 'Привет! Купи слона!'
-        res['response']['buttons'] = get_suggests(user_id)
-        return
-    test = [
-        'ладно',
-        'куплю',
-        'покупаю',
-        'хорошо'
-    ]
-    if any(i in req['request']['original_utterance'].lower() for i in test):
-        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
-        response = {
-            'session': request.json['session'],
-            'version': request.json['version'],
-            'response': {
-                'end_session': False
-            }
-        }
-        handle_dialog_crol(request.json, response)
-        return
+    else:
+        if not sessionStorage[user_id]['game_started']:
+            if 'РґР°' in req['request']['nlu']['tokens']:
+                if len(sessionStorage[user_id]['guessed_cities']) == 3:
+                    res['response']['text'] = 'РўС‹ РѕС‚РіР°РґР°Р» РІСЃРµ РіРѕСЂРѕРґР°!'
+                    res['end_session'] = True
+                else:
+                    sessionStorage[user_id]['game_started'] = True
+                    sessionStorage[user_id]['attempt'] = 1
+                    play_game(res, req)
+            elif 'РЅРµС‚' in req['request']['nlu']['tokens']:
+                res['response']['text'] = 'РќСѓ Рё Р»Р°РґРЅРѕ!'
+                res['end_session'] = True
+            else:
+                res['response']['text'] = 'РќРµ РїРѕРЅСЏР»Р° РѕС‚РІРµС‚Р°! РўР°Рє РґР° РёР»Рё РЅРµС‚?'
+                res['response']['buttons'] = [
+                    {
+                        'title': 'Р”Р°',
+                        'hide': True
+                    },
+                    {
+                        'title': 'РќРµС‚',
+                        'hide': True
+                    }
+                ]
+        else:
+            play_game(res, req)
 
-    res['response']['text'] = \
-        f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
-    res['response']['buttons'] = get_suggests(user_id)
 
-
-def handle_dialog_crol(req, res):
+def play_game(res, req):
     user_id = req['session']['user_id']
+    attempt = sessionStorage[user_id]['attempt']
+    if attempt == 1:
+        city = random.choice(list(cities))
+        while city in sessionStorage[user_id]['guessed_cities']:
+            city = random.choice(list(cities))
+        sessionStorage[user_id]['city'] = city
+        res['response']['card'] = {}
+        res['response']['card']['type'] = 'BigImage'
+        res['response']['card']['title'] = 'Р§С‚Рѕ СЌС‚Рѕ Р·Р° РіРѕСЂРѕРґ?'
+        res['response']['card']['image_id'] = cities[city][attempt - 1]
+        res['response']['text'] = 'РўРѕРіРґР° СЃС‹РіСЂР°РµРј!'
+    else:
+        city = sessionStorage[user_id]['city']
+        if get_city(req) == city:
+            res['response']['text'] = 'РџСЂР°РІРёР»СЊРЅРѕ! РЎС‹РіСЂР°РµРј РµС‰С‘?'
+            sessionStorage[user_id]['guessed_cities'].append(city)
+            sessionStorage[user_id]['game_started'] = False
+            return
+        else:
+            if attempt == 3:
+                res['response']['text'] = f'Р’С‹ РїС‹С‚Р°Р»РёСЃСЊ. Р­С‚Рѕ {city.title()}. РЎС‹РіСЂР°РµРј РµС‰С‘?'
+                sessionStorage[user_id]['game_started'] = False
+                sessionStorage[user_id]['guessed_cities'].append(city)
+                return
+            else:
+                res['response']['card'] = {}
+                res['response']['card']['type'] = 'BigImage'
+                res['response']['card']['title'] = 'РќРµРїСЂР°РІРёР»СЊРЅРѕ. Р’РѕС‚ С‚РµР±Рµ РґРѕРїРѕР»РЅРёС‚РµР»СЊРЅРѕРµ С„РѕС‚Рѕ'
+                res['response']['card']['image_id'] = cities[city][attempt - 1]
+                res['response']['text'] = 'Рђ РІРѕС‚ Рё РЅРµ СѓРіР°РґР°Р»!'
+    sessionStorage[user_id]['attempt'] += 1
 
-    if req['session']['new']:
-        sessionStorage[user_id] = {
-            'suggests': [
-                "Не хочу.",
-                "Не буду.",
-                "Отстань!",
-            ]
-        }
-        res['response']['text'] = 'Привет! Купи кролика!'
-        res['response']['buttons'] = get_suggests(user_id)
-        return
-    test = [
-        'ладно',
-        'куплю',
-        'покупаю',
-        'хорошо'
-    ]
-    if any(i in req['request']['original_utterance'].lower() for i in test):
-        res['response']['text'] = 'Кролика можно найти на Яндекс.Маркете!'
-        res['response']['end_session'] = True
-        return
 
-    res['response']['text'] = \
-        f"Все говорят '{req['request']['original_utterance']}', а ты купи кролика!"
-    res['response']['buttons'] = get_suggests(user_id)
+def get_city(req):
+    for entity in req['request']['nlu']['entities']:
+        if entity['type'] == 'YANDEX.GEO':
+            return entity['value'].get('city', None)
 
 
-def get_suggests(user_id):
-    session = sessionStorage[user_id]
-    suggests = [
-        {'title': suggest, 'hide': True}
-        for suggest in session['suggests'][:2]
-    ]
-
-    session['suggests'] = session['suggests'][1:]
-    sessionStorage[user_id] = session
-
-    if len(suggests) < 2:
-        suggests.append({
-            "title": "Ладно",
-            "url": "https://market.yandex.ru/search?text=слон",
-            "hide": True
-        })
-
-    return suggests
+def get_first_name(req):
+    for entity in req['request']['nlu']['entities']:
+        if entity['type'] == 'YANDEX.FIO':
+            return entity['value'].get('first_name', None)
 
 
 if __name__ == '__main__':
---
+++ вк/slon.py
@@ -0,0 +1,113 @@
+from flask import Flask, request, jsonify
+import logging
+
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
+
+sessionStorage = {}
+
+
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info(f'Request: {request.json!r}')
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+    handle_dialog(request.json, response)
+    logging.info(f'Response:  {response!r}')
+    return jsonify(response)
+
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ]
+        }
+        res['response']['text'] = 'Привет! Купи слона!'
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+    test = [
+        'ладно',
+        'куплю',
+        'покупаю',
+        'хорошо'
+    ]
+    if any(i in req['request']['original_utterance'].lower() for i in test):
+        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
+        response = {
+            'session': request.json['session'],
+            'version': request.json['version'],
+            'response': {
+                'end_session': False
+            }
+        }
+        handle_dialog_crol(request.json, response)
+        return
+
+    res['response']['text'] = \
+        f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
+    res['response']['buttons'] = get_suggests(user_id)
+
+
+def handle_dialog_crol(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ]
+        }
+        res['response']['text'] = 'Привет! Купи кролика!'
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+    test = [
+        'ладно',
+        'куплю',
+        'покупаю',
+        'хорошо'
+    ]
+    if any(i in req['request']['original_utterance'].lower() for i in test):
+        res['response']['text'] = 'Кролика можно найти на Яндекс.Маркете!'
+        res['response']['end_session'] = True
+        return
+
+    res['response']['text'] = \
+        f"Все говорят '{req['request']['original_utterance']}', а ты купи кролика!"
+    res['response']['buttons'] = get_suggests(user_id)
+
+
+def get_suggests(user_id):
+    session = sessionStorage[user_id]
+    suggests = [
+        {'title': suggest, 'hide': True}
+        for suggest in session['suggests'][:2]
+    ]
+
+    session['suggests'] = session['suggests'][1:]
+    sessionStorage[user_id] = session
+
+    if len(suggests) < 2:
+        suggests.append({
+            "title": "Ладно",
+            "url": "https://market.yandex.ru/search?text=слон",
+            "hide": True
+        })
+
+    return suggests
+
+
+if __name__ == '__main__':
+    app.run()
